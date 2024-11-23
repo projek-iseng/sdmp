a27b4c970a1762b15697bf8d0d7b49a1a6d3f198
piyo12052004 <piyoaswandi@gmail.com>
2024-11-24 00:05:46 +0700

    fornd end pada master registrasi user`s

--- frontend/src/views/master/master-registrasi.vue
+++ frontend/src/views/master/master-registrasi.vue
@@ -2,24 +2,53 @@
 import apiClient from "../../services/apiService";
 import { ref, onMounted } from "vue";
 import imgLogo from '../../../public/game.svg';
+import { ProductService } from "../../services/ProductService";
 
+const jabatanData = ref([]);
+const departemenData = ref([]);
+const data = ref([]);
+const search = ref([]);
+const loading = ref(false)
+const submitted = ref(false);
 const productDialog = ref(false);
+const tglLahir = ref(null);
+
 
 const dialogCreate  = async ()=> {
     productDialog.value = true
+}
+const saveUsers = async ()=> {
+    productDialog.value = false
 }
 
 const hideDialog = () => {
   productDialog.value = false;
 };
 
+const jabatanDanDepartemenDataPegawai = async () => {
+  try {
+    const responseJabatan = await apiClient.get("/api/jabatan");
+    jabatanData.value = responseJabatan.data.data.map((item) => ({
+      label: item.namaJabatan,
+      value: item.id,
+    }));
+    const responseDepartemen = await apiClient.get("/api/departemen");
+    departemenData.value = responseDepartemen.data.data.map((item) => ({
+      label: item.namaDepartemen,
+      value: item.id,
+    }));
+  } catch (error) {
+    console.error("Error loading data:", error);
+  }
+};
+onMounted(() => {
+  jabatanDanDepartemenDataPegawai();
+});
 </script>
 
 <template>
     <div>
-        <div class="flex align-items-end justify-content-end" style="padding:10px">
-            <Button label="Create" icon="pi pi-plus" @click="dialogCreate" iconPos="right" class="buttonCreatePegawai" />
-        </div>
+       
         <Splitter class="splitter-custom" layout="horizontal">
             <SplitterPanel class="flex align-items-center justify-content-center no-resize" :size="65" > 
                 <div class="formgrid grid">
@@ -56,53 +85,159 @@ const hideDialog = () => {
                     </div>
                 </div>
         </SplitterPanel>
-            <SplitterPanel class="flex align-items-center justify-content-center no-resize" :size="35" style="padding:10px;" >
+            <SplitterPanel class=" no-resize" :size="35" style="padding:20px;" >
+                <div class="text-center mb-5">
+                    <label for="" style="font-size:20px; font-weight: bold; padding:10px">Search User's dan Creact User's</label>
+                </div>
                 <div class="formgrid grid">
                     <div class="field col">
                         <IconField iconPosition="left">
                             <InputIcon class="pi pi-search"> </InputIcon>
-                            <InputText v-model="value1" placeholder="Nama Pegawai" />
+                            <InputText v-model="search.namapegawai" placeholder="Nama Pegawai" />
                         </IconField>
                     </div>
                     <div class="field col">
                         <IconField iconPosition="left">
                             <InputIcon class="pi pi-search"> </InputIcon>
-                            <InputText v-model="value1" placeholder="Id Pegawai" />
+                            <InputText v-model="search.idPegawai" placeholder="Id Pegawai" />
                         </IconField>
                     </div>
                 </div>
                 <div class="formgrid grid">    
-                    <div class="field grid">
+                    <div class="field col">
                         <IconField iconPosition="left">
                             <InputIcon class="pi pi-search"> </InputIcon>
-                            <InputText v-model="value1" placeholder="Jabatan" />
+                            <InputText v-model="search.jabatan" placeholder="Jabatan" />
                         </IconField>
                     </div>
-                    <div class="field grid">
+                    <div class="field col">
                         <IconField iconPosition="left">
                             <InputIcon class="pi pi-search"> </InputIcon>
-                            <InputText v-model="value1" placeholder="Departemen" />
+                            <InputText v-model="search.departemen" placeholder="Departemen" />
                         </IconField>
                     </div>
                 </div>
-                <div class="field">
-                <Button type="button" label="Search" icon="pi pi-search" :loading="loading" @click="load" />
+                <div class="fromgrid grid">
+                    <div class="field col">
+                    <Button type="button" label="Search User's" icon="pi pi-search"  class="w-10" :loading="loading" />
+                    </div>
+                     <div class="field col" >
+                        <Button label="Create Users" icon="pi pi-plus" @click="dialogCreate" iconPos="left" class=" w-10" />
+                    </div>
                 </div>
             </SplitterPanel>
         </Splitter>
         <Dialog
-        v-model:visible="productDialog"
-        :style="{ width: '1050px' }"
-        header="Registrasi Pegawai"
-        :modal="true"
-        class="p-fluid"
+      v-model:visible="productDialog"
+      :style="{ width: '1050px' }"
+      header="Nama Pegawai"
+      :modal="true"
+      class="p-fluid"
+    >
+      <div class="formgrid grid">
+        <div class="field col">
+          <label for="name">Nama Lengkap</label>
+          <InputText
+            id="name"
+            v-model.trim="data.namaPegawai "
+            required="true"
+            autofocus
+            autocomplete="off"
+            :invalid="submitted && !data.namaPegawai"
+          />
+          <small class="p-error" v-if="submitted && !data.namaPegawai"
+            >Nama Lengkap diperlukan.</small
+          >
+        </div>
+        <div class="field col">
+          <label for="email">Email</label>
+          <InputText
+            id="email"
+            v-model="data.email"
+            required="true"
+            autocomplete="off"
+            :invalid="submitted && !data.email"
+          />
+          <small class="p-error" v-if="submitted && !data.email">Email diperlukan.</small>
+        </div>
+        <div class="field col">
+          <label for="noHp">No Handphone</label>
+          <InputText
+            id="noHp"
+            v-model="data.noHp"
+            required="true"
+            autocomplete="off"
+            :invalid="submitted && !data.noHp"
+          />
+          <small class="p-error" v-if="submitted && !data.noHp"
+            >No Handphone diperlukan.</small
+          >
+        </div>
+      </div>
+      <div class="field">
+        <label for="alamat">Alamat</label>
+        <Textarea
+          id="alamat"
+          v-model.trim="data.alamat"
+          rows="5"
+          cols="30"
+          autocomplete="off"
+          :invalid="submitted && !data.alamat"
+        />
+        <small class="p-error" v-if="submitted && !data.alamat">Alamat diperlukan.</small>
+      </div>
+      <div class="formgrid grid">
+      <div class="field col">
+        <label for="jabatan">Jabatan</label>
+        <Dropdown
+          id="jabatan"
+          v-model="data.jabatan"
+          :options="jabatanData"
+          optionLabel="label"
+          optionValue="value"
+          placeholder="Pilih Jabatan"
+          :invalid="submitted && !data.jabatan"
+        />
+        <small class="p-error" v-if="submitted && !data.jabatan"
+          >Jabatan diperlukan.</small
         >
-        
-        <template #footer>
-            <Button label="Cancel" icon="pi pi-times" text @click="hideDialog" />
-            <Button label="Save" icon="pi pi-check" text @click="savePegawai" />
-        </template>
-        </Dialog>
+      </div>
+      <div class="field col">
+        <label for="jabatan">Departemen</label>
+        <Dropdown
+          id="jabatan"
+          v-model="data.departemen"
+          :options="departemenData"
+          optionLabel="label"
+          optionValue="value"
+          placeholder="Pilih Jabatan"
+          :invalid="submitted && !data.departemen"
+        />
+       <small class="p-error" v-if="submitted && !data.departemen"
+          >Departemen diperlukan.</small
+        >
+      </div>
+      </div>
+      <div class="formgrid grid">
+        <div class="field col">
+            <label for="username">Username</label>
+           <FloatLabel>
+                <InputText autocomplete="off" id="username" v-model="data.username" />
+            </FloatLabel>
+        </div>
+        <div class="field col">
+            <label for="password">Password</label>
+           <FloatLabel>
+                <InputText autocomplete="off" id="password" v-model="data.password" />
+            </FloatLabel>
+        </div>
+      </div>
+      
+      <template #footer>
+        <Button label="Cancel" icon="pi pi-times" text @click="hideDialog" />
+        <Button label="Create" icon="pi pi-plus" text @click="saveUsers" />
+      </template>
+    </Dialog>
     </div>
 </template>
  
@@ -116,9 +251,5 @@ const hideDialog = () => {
   user-select: none; 
 }
 
-.buttonCreatePegawai{
-    width: 150px;
-    height: 35px;
-}
 
 </style>
